7c65f178e8db5e1d727e91bda86b814529f59d12
Don Han <5600864+don-han@users.noreply.github.com>
2019-12-12 23:48:34 -0800

    Initial commit

---
+++ with.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+git_with=$HOME/.git_with
+
+authors=$git_with"/authors.csv"
+git_template=$git_with"/template"
+
+args="$@"
+
+# Reset template
+> $git_template
+
+# Add two empty lines
+echo >> $git_template
+echo >> $git_template
+
+for nickname in $args
+do
+    author=$(grep $nickname $authors)
+    message=$(awk -F ',' '{ print "Co-authored-by: " $2 " " "<" $3 ">" }' <<< "$author" >> "$git_template") 
+done
+
+cat $git_template
+
+
+
+
