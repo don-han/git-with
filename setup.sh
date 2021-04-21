a7b3453535157eed83411db47a4efc3b210ad26f
Don Han <5600864+don-han@users.noreply.github.com>
2021-04-21 15:49:26 -0700

    Put alias in profile instead of bash profile
    This extends the functionality to *sh including zsh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 mv ../git-with ~/.git-with
 git config --global commit.template ~/.git-with/template
-echo $'alias with=\"bash ~/.git-with/with.sh\"' >> ~/.bash_profile 
-source ~/.bash_profile
+echo $'alias with=\"bash ~/.git-with/with.sh\"' >> ~/.profile 
+source ~/.profile
 
