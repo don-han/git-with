05dbb75e83ee5836b32727ffbea12988701e3b3b
Don Han <dhan@apple.com>
2021-04-26 11:34:17 -0700

    Add rdar to commit message if it exists
    Co-authored-by: Abbie Williams <abbie.williams@thoughtworks.com>

--- with.sh
+++ with.sh
@@ -10,6 +10,14 @@ args="$@"
 # Reset template
 > $git_template
 
+# If arg starts with rdar, add it to the commit message
+if [[ $1 =~ ^rdar ]] ;
+then
+    echo "<$1> " >> $git_template
+    shift # Shift argument positions by one to the right
+    args="$@"
+fi
+
 # Add two empty lines
 echo >> $git_template
 echo >> $git_template
