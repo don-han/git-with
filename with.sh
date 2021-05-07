3be68bd435c68b5fec28f899b26acc3cc69c1909
Don Han <dhan@apple.com>
2021-05-07 15:27:21 -0700

    feat: Add placeholder for convention commit
    rdar://1234
    Co-authored-by: Ed Thome <ethome@thoughtworks.com>

--- with.sh
+++ with.sh
@@ -10,29 +10,28 @@ args="$@"
 # Reset template
 > $git_template
 
-echo >> $git_template
-echo >> $git_template
+echo "type(scope?): description" >> $git_template
 
 # If arg starts with rdar, add it to the commit message
 if [[ $1 =~ ^rdar ]] ;
 then
+    echo >> $git_template
     echo "$1" >> $git_template
     shift # Shift argument positions by one to the right
     args="$@"
 fi
 
-# Add two empty lines
-echo >> $git_template
-echo >> $git_template
-
 for nickname in $args
 do
     author=$(grep ^$nickname $authors)
-    message=$(awk -F ',' '{ print "Co-authored-by: " $2 " " "<" $3 ">" }' <<< "$author" >> "$git_template") 
+    message=$(awk -F ',' '{ print "Co-authored-by: " $2 " " "<" $3 ">" }' <<< "$author" >> "$git_template")
 done
 
+echo >> $git_template
+cat >> $git_template <<- END
+# Valid commit types: [build, ci, chore, docs, feat, fix, perf, refactor, revert, style, test]
+# Conventional commit rules: https://cheatography.com/albelop/cheat-sheets/conventional-commits/
+END
+
 cat $git_template
-
-
-
 
