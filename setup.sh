07d47f0ebe93afd05d7e697694492aad5617f481
Don Han <5600864+don-han@users.noreply.github.com>
2021-05-13 09:57:58 -0700

    Make with script executable

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 mv ../git-with ~/.git-with
+chmod +x ~/.git-with/with.sh
 git config --global commit.template ~/.git-with/template
 echo $'alias with=\"bash ~/.git-with/with.sh\"' >> ~/.profile 
 source ~/.profile
